b64e80a01ce48e3e8ed28209b4d43917b8aa4aa0
saidali777 <148846780+saidali777@users.noreply.github.com>
2025-05-20 20:47:47 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -1,1 +1,16 @@
+import motor.motor_asyncio
+from config import MONGO_URI
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+db = client.helper_bot_db
+users_collection = db.users
+
+async def add_user(user_id: int, username: str):
+    user = await users_collection.find_one({"_id": user_id})
+    if not user:
+        await users_collection.insert_one({"_id": user_id, "username": username})
+
+async def get_all_users():
+    cursor = users_collection.find({})
+    return [user async for user in cursor]
 
