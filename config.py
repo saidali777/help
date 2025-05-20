19d1b4ff1f3ac5b7ef12856b6d5af860e0256ca6
saidali777 <148846780+saidali777@users.noreply.github.com>
2025-05-20 21:16:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
+API_ID = int(os.getenv("API_ID", "12618934"))
 API_HASH = os.getenv("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7361211575:AAF4MMOh9QkJo0jIz20dXIhlJitqO5mekJk")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://AUSTINDB:AUSTINDB@cluster0.7uari85.mongodb.net/?retryWrites=true&w=majority")
