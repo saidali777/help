2d5b9a806618c7e6778b905a31b74a70022c1831
saidali777 <148846780+saidali777@users.noreply.github.com>
2025-05-20 20:59:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_uri")
-ADMINS = [123456789]  # Telegram user IDs of admins
+API_HASH = os.getenv("API_HASH", , "49aacd0bc2f8924add29fb02e20c8a16")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7361211575:AAF4MMOh9QkJo0jIz20dXIhlJitqO5mekJk")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://AUSTINDB:AUSTINDB@cluster0.7uari85.mongodb.net/?retryWrites=true&w=majority")
+ADMINS = [7837304801]  # Telegram user IDs of admins
 
