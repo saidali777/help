138747ccfb3cfb83a248a5e30bc517dafeccf4fb
saidali777 <148846780+saidali777@users.noreply.github.com>
2025-05-20 20:47:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "123456"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_uri")
+ADMINS = [123456789]  # Telegram user IDs of admins
 
